e0da0e23ad988f20875b4cfee629a12a32a4700b
Simon Hayoz <hayoz.simon@bluewin.ch>
2022-05-02 11:21:49 +0200

    Update with better query type and fix running with cc

--- java_sql_queries/src/main/scala/QueriesWithCC.scala
+++ java_sql_queries/src/main/scala/QueriesWithCC.scala
@@ -73,7 +73,7 @@ object QueriesWithCC {
     createDBFromFile("world.sql", ctx)
 
     assert(
-      ctx.run(queryCity.take(4)) ==
+      ctx.run(query[City].take(4)) ==
         Seq(
           City(1, "Kabul", "AFG", "Kabol", 1780000),
           City(2, "Qandahar", "AFG", "Qandahar", 237500),
@@ -82,7 +82,7 @@ object QueriesWithCC {
         )
     )
     assert(
-      ctx.run(queryCountry.take(1)) ==
+      ctx.run(query[Country].take(1)) ==
         Seq(
           Country(
             "AFG",
@@ -104,7 +104,7 @@ object QueriesWithCC {
         )
     )
     assert(
-      ctx.run(queryCountryLanguage.take(4)) ==
+      ctx.run(query[CountryLanguage].take(4)) ==
         Seq(
           CountryLanguage("AFG", "Pashto", true, 52.4),
           CountryLanguage("NLD", "Dutch", true, 95.6),
@@ -113,7 +113,7 @@ object QueriesWithCC {
         )
     )
 
-    val populationAboveNineMillion = ctx.run(queryCity.filter(_.population > 9000000))
+    val populationAboveNineMillion = ctx.run(query[City].filter(_.population > 9000000))
     assert(
       pprint.log(populationAboveNineMillion) ==
         List(
@@ -126,7 +126,7 @@ object QueriesWithCC {
         )
     )
 
-    val bigCitiesInChina = ctx.run(queryCity.filter(c => c.population > 5000000 && c.countryCode == "CHN"))
+    val bigCitiesInChina = ctx.run(query[City].filter(c => c.population > 5000000 && c.countryCode == "CHN"))
     assert(
       pprint.log(bigCitiesInChina) ==
         List(
@@ -136,7 +136,7 @@ object QueriesWithCC {
           City(1893, "Tianjin", "CHN", "Tianjin", 5286800)
         )
     )
-    val bigCitiesInChina2 = ctx.run(queryCity.filter(_.population > 5000000).filter(_.countryCode == "CHN"))
+    val bigCitiesInChina2 = ctx.run(query[City].filter(_.population > 5000000).filter(_.countryCode == "CHN"))
     assert(
       pprint.log(bigCitiesInChina2) ==
         List(
@@ -147,20 +147,20 @@ object QueriesWithCC {
         )
     )
 
-    def find(cityId: Int) = ctx.run(queryCity.filter(_.id == cityId))
+    def find(cityId: Int) = ctx.run(query[City].filter(_.id == cityId))
 
     assert(pprint.log(find(3208)) == List(City(3208, "Singapore", "SGP", "\u0096", 4017733)))
     assert(pprint.log(find(3209)) == List(City(3209, "Bratislava", "SVK", "Bratislava", 448292)))
 
-    def findName(cityId: Int) = ctx.run(queryCity.filter(_.id == cityId).map(_.name))
+    def findName(cityId: Int) = ctx.run(query[City].filter(_.id == cityId).map(_.name))
 
     assert(pprint.log(findName(3208)) == List("Singapore"))
     assert(pprint.log(findName(3209)) == List("Bratislava"))
 
     println("Inserting Test City...")
-    ctx.run(queryCity.insertValue(City(10000, "test", "TST", "Test County", 0)))
+    ctx.run(query[City].insertValue(City(10000, "test", "TST", "Test County", 0)))
 
-    val testCityInfo = ctx.run(queryCity.filter(_.population == 0))
+    val testCityInfo = ctx.run(query[City].filter(_.population == 0))
     assert(pprint.log(testCityInfo) == List(City(10000, "test", "TST", "Test County", 0)))
 
     println("Inserting More Test Cities...")
@@ -168,9 +168,9 @@ object QueriesWithCC {
       City(10001, "testville", "TSV", "Test County", 0),
       City(10002, "testopolis", "TSO", "Test County", 0),
       City(10003, "testberg", "TSB", "Test County", 0)
-    ).foreach(e => ctx.run(queryCity.insertValue(e))) // TODO extract the execute to the outside
+    ).foreach(e => ctx.run(query[City].insertValue(e))) // TODO extract the execute to the outside
 
-    val allTestCities = ctx.run(queryCity.filter(_.population == 0))
+    val allTestCities = ctx.run(query[City].filter(_.population == 0))
     assert(
       pprint.log(allTestCities) ==
         List(
@@ -182,21 +182,21 @@ object QueriesWithCC {
     )
 
     println("Updating testham City Info")
-    ctx.run(queryCity.filter(_.id == 10000).updateValue(City(10000, "testham", "TST", "Test County", 0)))
+    ctx.run(query[City].filter(_.id == 10000).updateValue(City(10000, "testham", "TST", "Test County", 0)))
 
-    val testhamCityInfo = ctx.run(queryCity.filter(_.id == 10000))
+    val testhamCityInfo = ctx.run(query[City].filter(_.id == 10000))
     assert(pprint.log(testhamCityInfo) == List(City(10000, "testham", "TST", "Test County", 0)))
 
     println("Updating testford City Info")
-    ctx.run(queryCity.filter(_.id == 10000).update("name" -> "testford"))
+    ctx.run(query[City].filter(_.id == 10000).update("name" -> "testford"))
 
-    val testfordCityInfo = ctx.run(queryCity.filter(_.id == 10000))
+    val testfordCityInfo = ctx.run(query[City].filter(_.id == 10000))
     assert(pprint.log(testfordCityInfo) == List(City(10000, "testford", "TST", "Test County", 0)))
 
     println("Updating all Test County Cities...")
-    ctx.run(queryCity.filter(_.district == "Test County").update("district" -> "Test Borough"))
+    ctx.run(query[City].filter(_.district == "Test County").update("district" -> "Test Borough"))
 
-    val updatedTestCountyCitiesInfo = ctx.run(queryCity.filter(_.population == 0))
+    val updatedTestCountyCitiesInfo = ctx.run(query[City].filter(_.population == 0))
     assert(
       pprint.log(updatedTestCountyCitiesInfo) ==
         List(
--- java_sql_queries/src/main/scala/Query.scala
+++ java_sql_queries/src/main/scala/Query.scala
@@ -1,188 +1,194 @@
 import QueriesWithCC.{City, Country, CountryLanguage}
 
 import java.sql.{Connection, PreparedStatement, ResultSet, Types}
+import scala.reflect.ClassTag
 
 extension (c: Connection)
+/**
+ * Run a query
+ *
+ * @param q the query to run
+ * @return a sequence of element of type A
+ */
+  def run[A, B](q: {*} SQLQuery[A, B] ): B = q.run (c)
+
+case class DataTable[A](tableName: String, fromResultSet: ResultSet => A, columns: List[String], toPreparedStatement: (A, PreparedStatement, Int) => Unit)
 
 /**
- * Run a function using the current connection
+ * Top most level SQL query
  *
- * @return a return type
+ * @param dataTable the dataTable containing all information for retrieving and pushing data to the underlying DB
+ * @tparam A the type of the element in this Query
+ * @tparam B the result type of the query
  */
-  def run[A](q: Connection => A): A = q(c)
-  /**
-   * Run a query
-   *
-   * @param q the query to run
-   * @return a sequence of element of type A
-   */
-  def run[A](q: {*} Query[A]): Seq[A] = q.run(c)
-
-abstract class Query[A](tableName: String, fromResultSet: ResultSet => A, columns: List[String], toPreparedStatement: (A, PreparedStatement, Int) => Unit) {
-  /**
-   * Whether this query already has data that can be pipelined
-   *
-   * @return whether this query already has data that can be pipelined
-   */
-  def hasData: Boolean = false
-
+abstract class SQLQuery[A, B](dataTable: DataTable[A]) {
   /**
    * Run a query using the connection
    *
    * @param ctx the connection to run the query from
    * @return a sequence of element of type A
    */
-  def run(ctx: Connection): Seq[A]
+  def run(ctx: Connection): B
+}
+
+/**
+ * Pipelined query
+ *
+ * @param dataTable the dataTable containing all information for retrieving and pushing data to the underlying DB
+ * @tparam A the type of the element in this Query
+ */
+abstract class PipelinedQuery[A](dataTable: DataTable[A]) extends SQLQuery[A, Seq[A]](dataTable) {
+  this: {*} PipelinedQuery[A] =>
 
   /**
-   * Create a new take query taking n elements either from a previous query or from the underlying table
+   * Create a new take query taking n elements from a previous query
    *
    * @param n the number of elements to get
-   * @return a new query pipelined from the current one
+   * @return a new query, pipelined from the current one
    */
-  def take(n: Int): {*} Query[A] = TakeQuery(tableName, fromResultSet, columns, toPreparedStatement, this, n)
+  def take(n: Int): {this} PipelinedQuery[A] = TakeQuery(n, this, dataTable)
 
   /**
-   * Create a new filter query that filters either from a previous query or from the underlying table
+   * Create a new filter query filtering from a previous query using the predicate pred
    *
-   * @param pred the predicate to filter on
-   * @return a new query pipelined from the current one
+   * @param pred the predicate
+   * @return a new query, pipelined from the current one
    */
-  def filter(pred: A => Boolean): {*} Query[A] = FilterQuery(tableName, fromResultSet, columns, toPreparedStatement, this, pred)
+  def filter(pred: A => Boolean): {pred, this} PipelinedQuery[A] = FilterQuery(pred, this, dataTable)
 
   /**
-   * Update all values from the current query with the new value
+   * Create a new update value query updating all values from a previous query using the value
    *
-   * @param value the new value
-   * @return a function to be applied to execute the new query
+   * @param value the value
+   * @return a new query, pipelined from the current one
    */
-  def updateValue(value: A): Connection => Int = {
-    if (this.hasData) {
-      return ctx => {
-        ctx.run(this).map(p => {
-          val stmt: PreparedStatement = ctx.prepareStatement(s"UPDATE $tableName SET ${columns.map(_ + " = ?").mkString(", ")} WHERE ${columns.map(_ + " = ?").mkString(" AND ")}")
-          toPreparedStatement(value, stmt, 0)
-          toPreparedStatement(p, stmt, columns.length)
-          stmt.executeUpdate()
-        }).head // TODO use list of query to query of list
-      }
+  def updateValue(value: A): {this} SQLQuery[A, Int] = UpdateValueQuery(value, this, dataTable)
+
+  /**
+   * Create a new update value query updating all values from a previous query using the value
+   *
+   * @param updateTuple the tuple of (columnName, newValue)
+   * @return a new query, pipelined from the current one
+   */
+  def update(updateTuple: (String, String)): {this} SQLQuery[A, Int] = UpdateQuery(updateTuple, this, dataTable)
+
+  /**
+   * Create a new map query transforming all values from the previous query
+   *
+   * @param f the transform function
+   * @return a new query, pipelined from the current one
+   */
+  def map[B](f: A => B): {f, this} SQLQuery[A, Seq[B]] = MapQuery(f, this, dataTable)
+}
+
+/**
+ * Non pipelined query, represents the start of a query pipeline as it has no previous element
+ * @param dataTable the dataTable containing all information for retrieving and pushing data to the underlying DB
+ * @tparam A the type of the element in this Query
+ */
+abstract class NonPipelinedQuery[A](dataTable: DataTable[A]) extends SQLQuery[A, Seq[A]](dataTable) {
+  /**
+   * Create a new take query taking n elements from the underlying DB
+   *
+   * @param n the number of elements to get
+   * @return a new query, pipelined from the current one
+   */
+  def take(n: Int): PipelinedQuery[A] = TakeQueryFromDataTable(n, dataTable)
+
+  /**
+   * Create a new filter query filtering from the underlying DB using the predicate pred
+   *
+   * @param pred the predicate
+   * @return a new query, pipelined from the current one
+   */
+  def filter(pred: A => Boolean): {pred} PipelinedQuery[A] = FilterQueryFromDataTable(pred, dataTable)
+
+  /**
+   * Create a new insert value query adding a new value in the underlying DB
+   *
+   * @param value the value
+   * @return a new query, pipelined from the current one
+   */
+  def insertValue(value: A): SQLQuery[A, Int] = InsertValueQuery(value, dataTable)
+}
+
+/**
+ * Datatable query containing the information of the DB
+ * @param dataTable the dataTable containing all information for retrieving and pushing data to the underlying DB
+ * @tparam A the type of the element in this Query
+ */
+case class DataTableQuery[A](dataTable: DataTable[A]) extends NonPipelinedQuery[A](dataTable) {
+  override def run(ctx: Connection): Seq[A] = ???
+}
+
+case class FilterQuery[A](pred: A => Boolean, prev: {*} PipelinedQuery[A], dataTable: DataTable[A]) extends PipelinedQuery[A](dataTable: DataTable[A]) {
+  override def run(ctx: Connection): Seq[A] = prev.run(ctx).filter(pred)
+}
+
+case class FilterQueryFromDataTable[A](pred: A => Boolean, dataTable: DataTable[A]) extends PipelinedQuery[A](dataTable) {
+  override def run(ctx: Connection): Seq[A] = {
+    val stmt: PreparedStatement = ctx.prepareStatement(s"SELECT * FROM ${dataTable.tableName}")
+    val rs: ResultSet = stmt.executeQuery()
+    var finalSeq: Seq[A] = Seq()
+    while (rs.next()) {
+      val newValue = dataTable.fromResultSet(rs)
+      if (pred(newValue))
+        finalSeq = finalSeq :+ dataTable.fromResultSet(rs)
     }
-    throw RuntimeException("Nothing to update")
+    finalSeq
   }
+}
 
-  /**
-   * Update all values from the current queries with a certain field changed
-   *
-   * @param updateTuple the fields and its associated value to be updated
-   * @return a function to be applied to execute the new query
-   */
-  def update[B](updateTuple: (String, String)): Connection => Int = {
-    //    println(classOf[City].getDeclaredFields.map(e => (e.getName.toLowerCase, e.getType)).toList)
-    if (this.hasData) {
-      return ctx => {
-        ctx.run(this).map(p => {
-          val stmt: PreparedStatement = ctx.prepareStatement(s"UPDATE $tableName SET ${updateTuple._1} = ? WHERE ${columns.map(_ + " = ?").mkString(" AND ")}")
-          stmt.setString(1, updateTuple._2)
-          toPreparedStatement(p, stmt, 1)
-          stmt.executeUpdate()
-        }).head // TODO use list of query to query of list
-      }
+case class TakeQuery[A](n: Int, prev: {*} PipelinedQuery[A], dataTable: DataTable[A]) extends PipelinedQuery[A](dataTable) {
+  override def run(ctx: Connection): Seq[A] = prev.run(ctx).take(n)
+}
+
+case class TakeQueryFromDataTable[A](n: Int, dataTable: DataTable[A]) extends PipelinedQuery[A](dataTable) {
+  override def run(ctx: Connection): Seq[A] = {
+    val stmt: PreparedStatement = ctx.prepareStatement(s"SELECT * FROM ${dataTable.tableName} LIMIT ?")
+    stmt.setInt(1, n)
+    val rs: ResultSet = stmt.executeQuery()
+    var finalSeq: Seq[A] = Seq()
+    while (rs.next()) {
+      finalSeq = finalSeq :+ dataTable.fromResultSet(rs)
     }
-    throw RuntimeException("Nothing to update")
+    finalSeq
   }
+}
 
-  /**
-   * Map the result of the current query
-   *
-   * @param f the function to be applied to every value
-   * @return a function to be applied to execute the new query
-   */
-  def map[B](f: A => B): Connection => Seq[B] = {
-    if (this.hasData) {
-      ctx => ctx.run(this).map(f)
-    } else {
-      _ => Seq()
-    }
-  }
+case class UpdateValueQuery[A](value: A, prev: {*} PipelinedQuery[A], dataTable: DataTable[A]) extends SQLQuery[A, Int](dataTable) {
+  override def run(ctx: Connection): Int = prev.run(ctx).map(p => {
+    val stmt: PreparedStatement = ctx.prepareStatement(s"UPDATE ${dataTable.tableName} SET ${dataTable.columns.map(_ + " = ?").mkString(", ")} WHERE ${dataTable.columns.map(_ + " = ?").mkString(" AND ")}")
+    dataTable.toPreparedStatement(value, stmt, 0)
+    dataTable.toPreparedStatement(p, stmt, dataTable.columns.length)
+    stmt.executeUpdate()
+  }).head // TODO use list of query to query of list
+}
 
-  /**
-   * Insert a new value
-   *
-   * @param value the value to be inserted
-   * @return a function to be applied to execute the new query
-   */
-  def insertValue(value: A): Connection => Int = ctx => {
-    val stmt: PreparedStatement = ctx.prepareStatement(s"INSERT INTO $tableName (${columns.mkString(", ")}) VALUES(${columns.map(_ => "?").mkString(", ")})")
-    toPreparedStatement(value, stmt, 0)
+case class UpdateQuery[A](updateTuple: (String, String), prev: {*} PipelinedQuery[A], dataTable: DataTable[A]) extends SQLQuery[A, Int](dataTable) {
+  override def run(ctx: Connection): Int = prev.run(ctx).map(p => {
+    val stmt: PreparedStatement = ctx.prepareStatement(s"UPDATE ${dataTable.tableName} SET ${updateTuple._1} = ? WHERE ${dataTable.columns.map(_ + " = ?").mkString(" AND ")}")
+    stmt.setString(1, updateTuple._2)
+    dataTable.toPreparedStatement(p, stmt, 1)
+    stmt.executeUpdate()
+  }).head // TODO use list of query to query of list
+}
+
+case class MapQuery[A, B](f: A => B, prev: {*} PipelinedQuery[A], dataTable: DataTable[A]) extends SQLQuery[A, Seq[B]](dataTable) {
+  override def run(ctx: Connection): Seq[B] = prev.run(ctx).map(f)
+}
+
+case class InsertValueQuery[A](value: A, dataTable: DataTable[A]) extends SQLQuery[A, Int](dataTable) {
+  override def run(ctx: Connection): Int = {
+    val stmt: PreparedStatement = ctx.prepareStatement(s"INSERT INTO ${dataTable.tableName} (${dataTable.columns.mkString(", ")}) VALUES(${dataTable.columns.map(_ => "?").mkString(", ")})")
+    dataTable.toPreparedStatement(value, stmt, 0)
     stmt.executeUpdate()
   }
 }
 
-/**
- * Empty query that should never be run
- */
-class EmptyQuery[A](tableName: String, fromResultSet: ResultSet => A, columns: List[String], toPreparedStatement: (A, PreparedStatement, Int) => Unit) extends Query[A](tableName, fromResultSet, columns, toPreparedStatement) {
-  override def run(ctx: Connection): Seq[A] = ???
-}
-
-/**
- * Take query that take up to n elements
- */
-case class TakeQuery[A](tableName: String, fromResultSet: ResultSet => A, columns: List[String], toPreparedStatement: (A, PreparedStatement, Int) => Unit,
-                        prev: {*} Query[A], n: Int) extends Query[A](tableName, fromResultSet, columns, toPreparedStatement) {
-  override def hasData: Boolean = true
-
-  override def run(ctx: Connection): Seq[A] = {
-    if (prev.hasData) {
-      prev.run(ctx).take(n)
-    } else {
-      val stmt: PreparedStatement = ctx.prepareStatement(s"SELECT * FROM $tableName LIMIT ?")
-      stmt.setInt(1, n)
-      val rs: ResultSet = stmt.executeQuery()
-      var finalSeq: Seq[A] = Seq()
-      while (rs.next()) {
-        finalSeq = finalSeq :+ fromResultSet(rs)
-      }
-      finalSeq
-    }
-  }
-}
-
-/**
- * Filter query that filter data
- */
-case class FilterQuery[A](tableName: String, fromResultSet: ResultSet => A, columns: List[String], toPreparedStatement: (A, PreparedStatement, Int) => Unit,
-                          prev: {*} Query[A], pred: A => Boolean) extends Query[A](tableName, fromResultSet, columns, toPreparedStatement) {
-  override def hasData: Boolean = true
-
-  override def run(ctx: Connection): Seq[A] = {
-    if (prev.hasData) {
-      prev.run(ctx).filter(pred)
-    } else {
-      val stmt: PreparedStatement = ctx.prepareStatement(s"SELECT * FROM $tableName")
-      val rs: ResultSet = stmt.executeQuery()
-      var finalSeq: Seq[A] = Seq()
-      while (rs.next()) {
-        val newValue = fromResultSet(rs)
-        if (pred(newValue))
-          finalSeq = finalSeq :+ fromResultSet(rs)
-      }
-      finalSeq
-    }
-  }
-}
-
-case class CityQuery() extends EmptyQuery[City]("city", rs => {
-  City(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5))
-}, List("id", "name", "countrycode", "district", "population"), (c, stmt, i) => {
-  stmt.setInt(1 + i, c.id)
-  stmt.setString(2 + i, c.name)
-  stmt.setString(3 + i, c.countryCode)
-  stmt.setString(4 + i, c.district)
-  stmt.setInt(5 + i, c.population)
-})
-
-def queryCity: Query[City] = CityQuery()
+val City_ : Class[City] = classOf[City]
+val Country_ : Class[Country] = classOf[Country]
+val CountryLanguage_ : Class[CountryLanguage] = classOf[CountryLanguage]
 
 extension (rs: ResultSet)
   def getOptional[A](f: ResultSet => A): Option[A] =
@@ -193,38 +199,47 @@ extension (rs: ResultSet)
       Some(vl)
     }
 
-case class CountryQuery() extends EmptyQuery[Country]("country", rs => {
-  Country(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getDouble(5),
-    rs.getOptional(_.getInt(6)), rs.getInt(7), rs.getOptional(_.getDouble(8)), rs.getOptional(_.getBigDecimal(9)),
-    rs.getOptional(_.getBigDecimal(10)), rs.getString(11), rs.getString(12), rs.getOptional(_.getString(13)), rs.getOptional(_.getInt(14)), rs.getString(15))
-}, List("code", "name", "continent", "region", "surfacearea", "indepyear", "population", "lifeexpectancy",
-  "gnp", "gnpold", "localname", "governmentform", "headofstate", "capital", "code2"), (c, stmt, i) => {
-  stmt.setString(1 + i, c.code)
-  stmt.setString(2 + i, c.name)
-  stmt.setString(3 + i, c.continent)
-  stmt.setString(4 + i, c.region)
-  stmt.setDouble(5 + i, c.surfaceArea)
-  if (c.indepYear.isDefined) stmt.setInt(6 + i, c.indepYear.get) else stmt.setNull(6 + i, Types.INTEGER)
-  stmt.setDouble(7 + i, c.population)
-  if (c.lifeExpectancy.isDefined) stmt.setDouble(8 + i, c.lifeExpectancy.get) else stmt.setNull(8 + i, Types.DOUBLE)
-  if (c.gnp.isDefined) stmt.setBigDecimal(9 + i, c.gnp.get.bigDecimal) else stmt.setNull(9 + i, Types.BIGINT)
-  if (c.gnpold.isDefined) stmt.setBigDecimal(10 + i, c.gnpold.get.bigDecimal) else stmt.setNull(10 + i, Types.BIGINT)
-  stmt.setString(11 + i, c.localName)
-  stmt.setString(12 + i, c.governmentForm)
-  if (c.headOfState.isDefined) stmt.setString(13 + i, c.headOfState.get) else stmt.setNull(13 + i, Types.VARCHAR)
-  if (c.capital.isDefined) stmt.setInt(14 + i, c.capital.get) else stmt.setNull(14 + i, Types.INTEGER)
-  stmt.setString(15 + i, c.code2)
-})
+// TODO if enough time generic case class to dataTable: val fields = ev.runtimeClass.getDeclaredFields.map(e => (e.getName.toLowerCase, e.getType)).toList
+def query[A](implicit ev: ClassTag[A]): DataTableQuery[A] = (ev.runtimeClass match {
+  case City_ => DataTableQuery[City](DataTable("city", rs => {
+    City(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5))
+  }, List("id", "name", "countrycode", "district", "population"), (c, stmt, i) => {
+    stmt.setInt(1 + i, c.id)
+    stmt.setString(2 + i, c.name)
+    stmt.setString(3 + i, c.countryCode)
+    stmt.setString(4 + i, c.district)
+    stmt.setInt(5 + i, c.population)
+  }))
 
-def queryCountry: Query[Country] = CountryQuery()
+  case Country_ => DataTableQuery[Country](DataTable("country", rs => {
+    Country(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getDouble(5),
+      rs.getOptional(_.getInt(6)), rs.getInt(7), rs.getOptional(_.getDouble(8)), rs.getOptional(_.getBigDecimal(9)),
+      rs.getOptional(_.getBigDecimal(10)), rs.getString(11), rs.getString(12), rs.getOptional(_.getString(13)), rs.getOptional(_.getInt(14)), rs.getString(15))
+  }, List("code", "name", "continent", "region", "surfacearea", "indepyear", "population", "lifeexpectancy",
+    "gnp", "gnpold", "localname", "governmentform", "headofstate", "capital", "code2"), (c, stmt, i) => {
+    stmt.setString(1 + i, c.code)
+    stmt.setString(2 + i, c.name)
+    stmt.setString(3 + i, c.continent)
+    stmt.setString(4 + i, c.region)
+    stmt.setDouble(5 + i, c.surfaceArea)
+    if (c.indepYear.isDefined) stmt.setInt(6 + i, c.indepYear.get) else stmt.setNull(6 + i, Types.INTEGER)
+    stmt.setDouble(7 + i, c.population)
+    if (c.lifeExpectancy.isDefined) stmt.setDouble(8 + i, c.lifeExpectancy.get) else stmt.setNull(8 + i, Types.DOUBLE)
+    if (c.gnp.isDefined) stmt.setBigDecimal(9 + i, c.gnp.get.bigDecimal) else stmt.setNull(9 + i, Types.BIGINT)
+    if (c.gnpold.isDefined) stmt.setBigDecimal(10 + i, c.gnpold.get.bigDecimal) else stmt.setNull(10 + i, Types.BIGINT)
+    stmt.setString(11 + i, c.localName)
+    stmt.setString(12 + i, c.governmentForm)
+    if (c.headOfState.isDefined) stmt.setString(13 + i, c.headOfState.get) else stmt.setNull(13 + i, Types.VARCHAR)
+    if (c.capital.isDefined) stmt.setInt(14 + i, c.capital.get) else stmt.setNull(14 + i, Types.INTEGER)
+    stmt.setString(15 + i, c.code2)
+  }))
 
-case class CountryLanguageQuery() extends EmptyQuery[CountryLanguage]("countrylanguage", rs => {
-  CountryLanguage(rs.getString(1), rs.getString(2), rs.getBoolean(3), rs.getDouble(4))
-}, List("countrycode", "language", "isofficial", "percentage"), (c, stmt, i) => {
-  stmt.setString(1 + i, c.countrycode)
-  stmt.setString(2 + i, c.language)
-  stmt.setBoolean(3 + i, c.isOfficial)
-  stmt.setDouble(4 + i, c.percentage)
-})
-
-def queryCountryLanguage: Query[CountryLanguage] = CountryLanguageQuery()
+  case CountryLanguage_ => DataTableQuery[CountryLanguage](DataTable("countrylanguage", rs => {
+    CountryLanguage(rs.getString(1), rs.getString(2), rs.getBoolean(3), rs.getDouble(4))
+  }, List("countrycode", "language", "isofficial", "percentage"), (c, stmt, i) => {
+    stmt.setString(1 + i, c.countrycode)
+    stmt.setString(2 + i, c.language)
+    stmt.setBoolean(3 + i, c.isOfficial)
+    stmt.setDouble(4 + i, c.percentage)
+  }))
+}).asInstanceOf[DataTableQuery[A]]
